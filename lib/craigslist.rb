63a2f48a373fd33ea8186f304f85f255cbdcf5cf
Ramon Chavez <rachavez1974@gmail.com>
2019-06-14 12:13:49 -0400

    Add private #cities_and_links to CL model and pass test

--- lib/craigslist.rb
+++ lib/craigslist.rb
@@ -15,4 +15,20 @@ class CraigsList
     doc.css(".colmask").first.css("h4").collect { |st| st.text}  
   end
 
+  private
+
+  def cities_and_links
+    #it returns a two dimensional array of cities, with their links as a hash by the state
+    # [[{"citya" => linka}, {"cityb" => linkb}, {"cityc" => linkc}], [{"cityz" => linkz}]]
+
+    two_dimension_city = []
+    doc.css(".colmask").first.css("ul").each do |city| 
+      #collect returns [{"city1" => link1}, {city2 => link2}]
+      #collect removes "/" and unnecessary spaces from cities
+      two_dimension_city.push(city.css("li a").collect { |city| { city.text.split("/")[0].strip  => city["href"] }})
+    end
+    two_dimension_city 
+  end
+
+
 end
