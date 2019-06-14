d1fe4aba7b2d991e8001d14b64b3e7883c561e87
Ramon Chavez <rachavez1974@gmail.com>
2019-06-14 11:42:34 -0400

    Add initialize() to CraigsList model, and pass test

--- lib/craigslist.rb
+++ lib/craigslist.rb
@@ -1,3 +1,14 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
 class CraigsList
+  attr_accessor :site_url, :doc
+  
+
+  def initialize()
+    @site_url = "https://www.craigslist.org/about/sites"
+    @doc = Nokogiri::HTML(open(@site_url))  
+  end
   
 end
